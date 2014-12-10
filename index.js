1fc2edb970c31cd970e7123aebae44df1242137f
morganherlocker <morgan.herlocker@gmail.com>
2014-12-09 19:15:04 -0500

    add benchmarking

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var extent = require('turf-extent')
-var bboxPolygon = require('turf-bbox-polygon')
+var extent = require('turf-extent');
+var bboxPolygon = require('turf-bbox-polygon');
 
 module.exports = function(features, done){
-  var bbox = extent(features)
-  var poly = bboxPolygon(bbox)
-  return poly
+  var bbox = extent(features);
+  var poly = bboxPolygon(bbox);
+  return poly;
 }
