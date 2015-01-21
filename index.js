f19c1789f036dab6864a76b44439c779db7da5a0
Tom MacWright <tom@macwright.org>
2015-01-21 12:13:38 -0800

    Fix point form

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var bboxPolygon = require('turf-bbox-polygon');
  * @param {FeatureCollection} fc a FeatureCollection of any type
  * @return {Polygon} a rectangular Polygon feature that encompasses all vertices
  * @example
- * var pt1 = turf.point(-75.343, 39.984, {name: 'Location A'});
- * var pt2 = turf.point(-75.833, 39.284, {name: 'Location B'});
- * var pt3 = turf.point(-75.534, 39.123, {name: 'Location C'});
+ * var pt1 = turf.point([-75.343, 39.984], {name: 'Location A'});
+ * var pt2 = turf.point([-75.833, 39.284], {name: 'Location B'});
+ * var pt3 = turf.point([-75.534, 39.123], {name: 'Location C'});
  * var fc = turf.featurecollection([pt1, pt2, pt3]);
  *
  * var enveloped = turf.envelope(fc);
