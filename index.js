44bb2943bc464cffc0dab8a71e0abc4d310ae685
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-03 16:35:26 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -8,16 +8,47 @@ var bboxPolygon = require('turf-bbox-polygon');
  * @param {FeatureCollection} fc a FeatureCollection of any type
  * @return {Polygon} a rectangular Polygon feature that encompasses all vertices
  * @example
- * var fc = turf.featurecollection([
- *  turf.point([-75.343, 39.984], {name: 'Location A'}),
- *  turf.point([-75.833, 39.284], {name: 'Location B'}),
- *  turf.point([-75.534, 39.123], {name: 'Location C'})
- * ]);
+ * var fc = {
+ *   "type": "FeatureCollection",
+ *   "features": [
+ *     {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "name": "Location A"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-75.343, 39.984]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "name": "Location B"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-75.833, 39.284]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "name": "Location C"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-75.534, 39.123]
+ *       }
+ *     }
+ *   ]
+ * };
  *
  * var enveloped = turf.envelope(fc);
  *
- * var result = turf.featurecollection(
- * 	fc.features.concat(enveloped));
+ * var resultFeatures = fc.features.concat(enveloped);
+ * var result = {
+ *   "type": "FeatureCollection",
+ *   "features": resultFeatures
+ * };
  *
  * //=result
  */
